cca3208d28513d35cf64263fdc13a382c5fea1da
Данијел Митровић <rodjenihm@outlook.com>
2019-06-18 08:38:38 +0200

    Implemented RpnPatterns.cs

---
+++ Rodjenihm.Lib.MojBroj/IRpnPatterns.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Rodjenihm.Lib.MojBroj
+{
+    internal interface IRpnPatterns
+    {
+        IEnumerable<int[]> CreateRpnPatternsForDigitCount(int count);
+    }
+}
---
+++ Rodjenihm.Lib.MojBroj/RpnPatterns.cs
@@ -0,0 +1,42 @@
+﻿using Rodjenihm.Lib.Combinatorics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rodjenihm.Lib.MojBroj
+{
+    internal sealed class RpnPatterns : IRpnPatterns
+    {
+        private bool IsValidRpnPattern(int[] pattern)
+        {
+            int stackCount = 0;
+
+            foreach (var token in pattern)
+            {
+                if (token == 1)
+                    stackCount++;
+                else if (token == 0)
+                {
+                    if (stackCount < 2)
+                        return false;
+                    stackCount--;
+                }
+            }
+
+            return stackCount == 1;
+        }
+
+        public IEnumerable<int[]> CreateRpnPatternsForDigitCount(int count)
+        {
+            var based = new int[2 * count - 1];
+
+            for (int i = 0; i < based.Length; i++)
+                based[i] = i < count ? 1 : 0;
+
+            foreach (var pattern in new Permutations<int>(based, 2, based.Length - 1))
+            {
+                if (IsValidRpnPattern(pattern.ToArray()))
+                    yield return pattern.ToArray();
+            }
+        }
+    }
+}
